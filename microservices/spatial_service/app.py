b07f990680d288c4b33743ac9b8094892b6790c3
Hemanth <hbayyarapu@deloitte.com.au>
2021-06-03 04:15:33 +1000

    Spatial service v2 - add another env variable

--- microservices/spatial_service/app.py
+++ microservices/spatial_service/app.py
@@ -3,12 +3,13 @@ from flask import Flask
 import os
 
 client_name = os.environ['client']
+s3_bucket = os.environ['s3_bucket']
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return f'Spatial Service - {client_name} - This is version 0.1.0'
+    return f'Spatial Service - {client_name} - {s3_bucket} This is version 0.2.0'
 
 @app.route('/service')
 def service():
