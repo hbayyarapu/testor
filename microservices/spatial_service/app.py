f83e7689848126ee27d09b19b4da51fc8e0c6aaa
Hemanth <hbayyarapu@deloitte.com.au>
2021-06-03 04:20:56 +1000

    Spatial service v2.1 - change attributes in return value

--- microservices/spatial_service/app.py
+++ microservices/spatial_service/app.py
@@ -9,10 +9,10 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return f'Spatial Service - {client_name} - {s3_bucket} This is version 0.2.0'
+    return f'Spatial Service - {client_name} - {s3_bucket} This is version 0.2.1'
 
 @app.route('/service')
 def service():
-    return {'value': 100 }
+    return {'weight': 100 , 'client': client_name}
 
 app.run(host='0.0.0.0', port=81)
