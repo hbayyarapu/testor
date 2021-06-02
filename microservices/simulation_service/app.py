73945bd07a6034497764f66ec8fd866ed7504eab
Hemanth <hbayyarapu@deloitte.com.au>
2021-06-03 05:29:13 +1000

    Simulation service v2.0 - Add new version of calculating

--- microservices/simulation_service/app.py
+++ microservices/simulation_service/app.py
@@ -13,7 +13,10 @@ def index():
 
 @app.route('/calculate')
 def calculate():
-    value = request.args.get('value')
+    value = request.args.get('weight')
+    client = request.args.get('client')
+    if client.lower() == "dot":
+        return value * 10
     return value * 20
 
 app.run(host='0.0.0.0', port=81)
