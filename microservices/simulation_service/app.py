64913029a10a3a78a37fcb9b489b93e567a38808
Hemanth <hbayyarapu@deloitte.com.au>
2021-06-03 05:28:13 +1000

    Simulation service v2.0 - Add endpoint to calculate

--- microservices/simulation_service/app.py
+++ microservices/simulation_service/app.py
@@ -1,4 +1,5 @@
 from flask import Flask
+from flask import request
 
 import os
 
@@ -9,5 +10,10 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     return f'Simulation Service - {client_name}- This is version 0.1.0'
+
+@app.route('/calculate')
+def calculate():
+    value = request.args.get('value')
+    return value * 20
 
 app.run(host='0.0.0.0', port=81)
